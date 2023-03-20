3739071258dcc9e8b657e282a13bb5bc85b98dd8
Praveen <46050644+rprav-n@users.noreply.github.com>
2023-03-21 00:00:15 +0530

    Add files via upload

---
+++ src/routes/guides/+layout.svelte
@@ -0,0 +1,30 @@
+<nav>
+    <div class="links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/guides">Guides</a>
+    </div>
+</nav>
+
+<main>
+    <slot></slot>
+</main>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: center;
+        background-color: rgb(45, 21, 76);
+        padding: 10px;
+    }
+    main {
+        max-width: 960px;
+        margin: 20px auto;
+    }
+    .links {
+        margin-left: auto;
+    }
+    a {
+        margin-left: 10px;
+    }
+</style>
---
+++ src/routes/guides/+page.svelte
@@ -0,0 +1,38 @@
+<script > // Add attribute called modules: Special Script
+  
+  let guides = [];
+
+  let getPosts = async() => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    guides = data;
+  }
+
+  getPosts();
+
+</script>
+
+
+<div class="guides">
+    <ul>
+      {#each guides as guide}
+        <li><a href="/">Guide {guide.title}</a></li>  
+      {/each}
+    </ul>
+  </div>
+  
+  <style>
+    .guides {
+      margin-top: 20px;
+    }
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    a {
+      display: inline-block;
+      margin-top: 10px;
+      padding: 10px;
+      border: 1px dotted rgba(255,255,255,0.2);
+    }
+  </style>
